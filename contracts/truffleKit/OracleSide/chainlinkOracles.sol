4797efa9794a7e976f78308c1fe94c758a32c639
juliustranquilli <kingofthecoast78@gmail.com>
2024-12-13 15:22:58 -0500

    update contracts/truffleKit/OracleSide/chainlinkOracles.sol: refactored SafeMath expressions

--- contracts/truffleKit/OracleSide/chainlinkOracles.sol
+++ contracts/truffleKit/OracleSide/chainlinkOracles.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import "../../oracle/oracle.sol";
-pragma solidity 0.6.12;
+pragma solidity ^0.8.0;
 
 contract EtherOracle is observerOracle {
     constructor(int256 _price)
