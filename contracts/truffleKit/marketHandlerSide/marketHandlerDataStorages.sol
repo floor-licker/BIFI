ba40905916704ea5ba5e5ea393d136aaefe501ee
juliustranquilli <kingofthecoast78@gmail.com>
2024-12-13 15:22:59 -0500

    update contracts/truffleKit/marketHandlerSide/marketHandlerDataStorages.sol: refactored SafeMath expressions

--- contracts/truffleKit/marketHandlerSide/marketHandlerDataStorages.sol
+++ contracts/truffleKit/marketHandlerSide/marketHandlerDataStorages.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import "../../marketHandler/marketHandlerDataStorage/handlerDataStorage.sol";
-pragma solidity 0.6.12;
+pragma solidity ^0.8.0;
 
 contract CoinHandlerDataStorage is marketHandlerDataStorage {
     constructor (uint256 _borrowLimit, uint256 _marginCallLimit, uint256 _minimumInterestRate, uint256 _liquiditySensitivity)
